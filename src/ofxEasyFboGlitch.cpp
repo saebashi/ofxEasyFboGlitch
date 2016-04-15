969ed7291bda38e7a91f66d48c902931894f4d6e
Andy McWilliams <andy@jahya.net>
2016-04-15 17:57:34 -0400

    Prevents having to write the buffer to disk

--- src/ofxEasyFboGlitch.cpp
+++ src/ofxEasyFboGlitch.cpp
@@ -4,7 +4,7 @@
 ofxEasyFboGlitch::ofxEasyFboGlitch(){
     fboW=320;
     fboH=240;
-    imgQuality =OF_IMAGE_QUALITY_WORST;
+    imgQuality=OF_IMAGE_QUALITY_WORST;
     fbo.allocate(fboW,fboH);
     glitchResetProbability=0.3;
 }
@@ -13,7 +13,6 @@ void ofxEasyFboGlitch::allocate(float _w,float _h){
     fboW=_w;
     fboH=_h;
     fbo.allocate(fboW,fboH);
-
 }
 
 //--------------------------------------------------------------
@@ -23,26 +22,23 @@ void ofxEasyFboGlitch::draw(ofFbo _fbo,float _drawX,float _drawY,float _drawW,fl
     ofBackground(0);
     _fbo.draw(0,0,fboW,fboH);
     fbo.end();
-        if(glitchReset){
-            reader.readToPixels(fbo, pix);
-            glitchImg.setFromPixels(pix);
-            glitchReset=false;
-        }
-        string breakImgName="glitch.jpg";
-        glitchImg.saveImage(breakImgName,imgQuality);
-        ofBuffer file= ofBufferFromFile(breakImgName);
-        int fileSize=file.size();
-        char *buffer = file.getBinaryBuffer();
-        int whichByte=(int)ofRandom(fileSize);
-        int whichBit =ofRandom(8);
-        char bitMask = 1<< whichBit;
-        buffer[whichByte] |= bitMask;
-        ofBufferToFile(breakImgName,file);
-        glitchImg.loadImage(breakImgName);
-        if (ofRandom(1)<glitchResetProbability) {
-            glitchReset=true;
-        }
-    glitchImg.draw(_drawX, _drawY, _drawW,_drawH);
+    if(glitchReset){
+        reader.readToPixels(fbo, pix);
+        glitchImg.setFromPixels(pix);
+        glitchReset=false;
+    }
+    ofSaveImage(glitchImg.getPixels(),buffer,OF_IMAGE_FORMAT_JPEG,imgQuality);
+    int fileSize=buffer.size();
+    int whichByte=(int)ofRandom(fileSize);
+    int whichBit=ofRandom(8);
+    char bitMask=1 << whichBit;
+    char* data=buffer.getData();
+    data[whichByte] |= bitMask;
+    glitchImg.load(buffer);
+    if (ofRandom(1)<glitchResetProbability) {
+        glitchReset=true;
+    }
+    glitchImg.draw(_drawX,_drawY,_drawW,_drawH);
 }
 //--------------------------------------------------------------
 void ofxEasyFboGlitch::setGlichResetProbability (float _probability){
